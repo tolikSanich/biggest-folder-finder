7e1eeb2c9f23d7207c2ee5839970625e258c2ca3
anatolii_levashev <toliksanich@gmail.com>
2021-10-22 10:09:53 +0500

    Initial added

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,17 @@ public class Main {
     public static void main(String[] args) {
         String folder ="E:\\фильмы";
         File file = new File(folder);
-
+        System.out.println(getSizeFolder(file));
 
     }
-    public static long sizeFolder(File folder) {
+    public static long getSizeFolder(File folder) {
         long sum = 0;
-        for (File file : folder.listFiles()) {
-            if (file.isDirectory()) {
-                sizeFolder(folder);
-                continue;
-            }
-            sum += file.length();
+        if (folder.isFile()){
+            return folder.length();
+        }
+        File[] files = folder.listFiles();
+        for (File file : files) {
+                sum += getSizeFolder(file);
         }
     return sum;
     }
