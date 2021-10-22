e6741fee160bf73d8c713c0a43c991dfe8eabd4b
anatolii_levashev <toliksanich@gmail.com>
2021-10-22 11:43:34 +0500

    add

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,29 @@
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
 
     public static void main(String[] args) {
-        String folder ="E:\\фильмы";
+
+        String folder ="E:\\Программы";
         File file = new File(folder);
+       long start = System.currentTimeMillis();
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+
+        System.out.println(size);
+        long duration = System.currentTimeMillis()-start;
+        System.out.println(duration);
+
+        long start1 = System.currentTimeMillis();
         System.out.println(getSizeFolder(file));
+        long duration1 = System.currentTimeMillis()-start1;
+        System.out.println(duration1);
 
     }
     public static long getSizeFolder(File folder) {
+
         long sum = 0;
         if (folder.isFile()){
             return folder.length();
@@ -17,6 +32,7 @@ public class Main {
         for (File file : files) {
                 sum += getSizeFolder(file);
         }
+
     return sum;
     }
 
